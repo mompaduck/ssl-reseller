5fefc4abf4e8e98e8b84eacc43d6fb643f398241
mompaduck <mompaduck@gmail.com>
2025-11-18 05:09:04 +0000

    Stimulus Loading 오류 수정

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,42 @@
 import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 import "@hotwired/turbo-rails"
 
+// Start Stimulus
 const application = Application.start()
+window.Stimulus = application
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus = application
 
-const context = require.context("./controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
+// Import and register all your controllers
+import AutoDismissController from "./controllers/auto_dismiss_controller.js"
+application.register("auto-dismiss", AutoDismissController)
+
+import DeleteConfirmController from "./controllers/delete_confirm_controller.js"
+application.register("delete-confirm", DeleteConfirmController)
+
+import EmailCheckController from "./controllers/email_check_controller.js"
+application.register("email-check", EmailCheckController)
+
+import FaqController from "./controllers/faq_controller.js"
+application.register("faq", FaqController)
+
+import HelloController from "./controllers/hello_controller.js"
+application.register("hello", HelloController)
+
+import MenuController from "./controllers/menu_controller.js"
+application.register("menu", MenuController)
+
+import PasswordConfirmController from "./controllers/password_confirm_controller.js"
+application.register("password-confirm", PasswordConfirmController)
+
+import PasswordFormController from "./controllers/password_form_controller.js"
+application.register("password-form", PasswordFormController)
+
+import PhoneValidateController from "./controllers/phone_validate_controller.js"
+application.register("phone-validate", PhoneValidateController)
+
+import TermsEnableController from "./controllers/terms_enable_controller.js"
+application.register("terms-enable", TermsEnableController)
 
 export { application }
--- app/javascript/controllers/application.js
+++
@@ -1,9 +0,0 @@
-import { Application } from "@hotwired/stimulus"
-
-
-const application = Application.start()
-window.Stimulus = application  // 필요시 디버그용
-
-export { application }
-
-// Stimulus 앱을 한 번 시작하고, 모든 컨트롤러 등록은 index.js에 집중시켜 관리
